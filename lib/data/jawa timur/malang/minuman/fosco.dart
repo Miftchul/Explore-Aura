26d1f03c64e3c300580bcc965b3aad5521a0f9e7
AlfinDioTriyuliarsha <alfindiot@gmail.com>
2025-05-20 20:05:45 +0700

    pembenahan1

--- lib/data/jawa timur/malang/minuman/fosco.dart
+++ lib/data/jawa timur/malang/minuman/fosco.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 class fosco extends StatefulWidget {
+  const fosco({super.key});
+
   @override
   State<fosco> createState() => _foscoState();
 }
